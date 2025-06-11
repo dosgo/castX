79ebe4e20905326e2ed904927a79e71de1b36d76
dosgo <dosgo@qq.com>
2025-06-11 21:44:36 +0800

    update go.mod

--- scrcpy.go
+++ scrcpy.go
@@ -2,25 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-	_ "net/http/pprof"
 
 	"github.com/dosgo/castX/scrcpy"
 )
 
 func main() {
-	StartHTTPServer()
 	scrcpyClient := scrcpy.NewScrcpyClient(8083, "test11", "", "1234561")
 	scrcpyClient.StartClient()
 	fmt.Scanln()
-}
-
-func StartHTTPServer() {
-	// 在独立goroutine中启动pprof
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
-
-	// 原有HTTP服务器启动代码...
 }
